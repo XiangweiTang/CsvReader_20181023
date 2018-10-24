13998507b4c523c034d55ee65a23c896292317c7
Xiangwei Tang <tangxiangwei@gmail.com>
2018-10-23 19:32:24 -0700

    Add project files.

---
+++ CsvReader_20181023/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CsvReader_20181023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csvPath = @"c:\1.csv";
+            var square = ReadCsv(csvPath);
+        }
+
+        static string[][] ReadCsv(string csvPath)
+        {
+            return File.ReadLines(csvPath).Select(x => x.Split(',')).ToArray();
+        }
+    }
+}
